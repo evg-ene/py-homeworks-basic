dca6820bf35de544f99c0a23f5b6069d05676332
Evgen <evg@example.com>
2022-02-07 00:59:03 +0300

    Add 'magic' in lesson 7

--- 6.classes/task_[1-3].py
+++ 6.classes/task_[1-3].py
@@ -8,6 +8,7 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
+        self.avg_grade = 0
     def rate_hw(self, lecturer, course, rate):
         if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress and rate <=10:
             if course in lecturer.rates:
@@ -22,7 +23,12 @@ class Student:
                  f'Средняя оценка за домашние задания: {sum(self.grades[self.courses_in_progress[0]]) / len(self.grades[self.courses_in_progress[0]])} \n'
                  f'Курсы в процессе изучения: {", ".join(self.courses_in_progress)} \n'
                  f'Завершенные курсы: {", ".join(self.finished_courses)}')
-        return result    
+        return result
+    def __lt__(self, other):
+        if not isinstance(other, Student):
+            print('Not a Student')
+            return
+        return self.avg_grade < other.avg_grade    
         
 class Mentor:
     def __init__(self, name, surname):
@@ -34,13 +40,19 @@ class Lecturer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
         self.rates = {}
+        self.avg_rate = 0
     def get_list(self):
         return [self.name, self.surname, self.courses_attached, self.rates]
     def __str__(self):
         result = (f'Имя: {self.name} \n'
                  f'Фамилия: {self.surname} \n'
-                 f'Средняя оценка за лекции: {sum(self.rates[self.courses_attached[0]]) / len(self.rates[self.courses_attached[0]])}')
+                 f'Средняя оценка за лекции: {self.avg_rate}')
         return result
+    def __lt__(self, other):
+        if not isinstance(other, Lecturer):
+            print('Not a Lecturer')
+            return
+        return self.avg_rate < other.avg_rate
 
 class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
@@ -56,22 +68,44 @@ class Reviewer(Mentor):
                  f'Фамилия: {self.surname}')
         return result
 
-best_student = Student('Arisa', 'Faned', 'female')
-best_student.courses_in_progress += ['Python', 'Git']
-best_student.finished_courses += ['Введение в программирование']
+best_student_one = Student('Arisa', 'Faned', 'female')
+best_student_one.courses_in_progress += ['Python', 'Git']
+best_student_one.finished_courses += ['Введение в программирование']
 
-best_lector = Lecturer('Lecter', 'Hannibal')
-best_lector.courses_attached += ['Python']
+best_student_two = Student('Men', 'Homo', 'male')
+best_student_two.courses_in_progress += ['Python', 'Git']
+best_student_two.finished_courses += ['Введение в программирование']
+
+best_lector_one = Lecturer('Lecter', 'Hannibal')
+best_lector_one.courses_attached += ['Python']
+
+best_lector_two = Lecturer('Hid', 'Aswer')
+best_lector_two.courses_attached += ['Python']
 
 reviewer_students = Reviewer('Rev', 'Ewer')
 reviewer_students.courses_attached += ['Python']
-reviewer_students.rate_hw(best_student, 'Python', 10)
-reviewer_students.rate_hw(best_student, 'Python', 9)
+reviewer_students.rate_hw(best_student_one, 'Python', 5)
+reviewer_students.rate_hw(best_student_one, 'Python', 4)
+reviewer_students.rate_hw(best_student_two, 'Python', 7)
+reviewer_students.rate_hw(best_student_two, 'Python', 5)
 
-rate_lector = best_student
-rate_lector.rate_hw(best_lector, 'Python', 10)
-rate_lector.rate_hw(best_lector, 'Python', 1)
+rate_lector = best_student_one
+rate_lector.rate_hw(best_lector_one, 'Python', 8)
+rate_lector.rate_hw(best_lector_one, 'Python', 3)
+best_lector_one.avg_rate = sum(best_lector_one.rates[best_lector_one.courses_attached[0]]) / len(best_lector_one.rates[best_lector_one.courses_attached[0]])
+rate_lector.rate_hw(best_lector_two, 'Python', 7)
+rate_lector.rate_hw(best_lector_two, 'Python', 5)
+best_lector_two.avg_rate = sum(best_lector_two.rates[best_lector_two.courses_attached[0]]) / len(best_lector_two.rates[best_lector_two.courses_attached[0]])
+
+print(best_lector_one.__lt__(best_lector_two))
 
 print(reviewer_students, '\n')
-print(best_lector, '\n')
-print(best_student, '\n')
+print(best_lector_one, '\n')
+print(best_student_one, '\n')
+
+
+print("Сравнение лекторов между собой по средней оценке:")
+print(best_lector_one.__lt__(best_lector_two))
+
+print("Сравнение студентов между собой по средней оценке:")
+print(best_student_two.__lt__(best_student_one))
